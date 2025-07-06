f3e062ed6fdadb0666e24d153c82a399b7c4cb47
Abdul Barr <abdulbarr730@gmail.com>
2025-07-07 00:07:49 +0530

    stage 3

--- app/api/budgets/[id]/route.ts
+++ app/api/budgets/[id]/route.ts
@@ -2,13 +2,11 @@ import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 import { NextRequest } from "next/server";
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(req: NextRequest, context: any) {
   const client = await clientPromise;
   const db = client.db();
-  const { id } = params;
+
+  const id = context.params.id;
 
   await db.collection("budgets").deleteOne({ _id: new ObjectId(id) });
 
@@ -17,21 +15,16 @@ export async function DELETE(
   });
 }
 
-export async function PUT(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(req: NextRequest, context: any) {
   const client = await clientPromise;
   const db = client.db();
-  const { id } = params;
 
+  const id = context.params.id;
   const { category, amount, month } = await req.json();
 
   await db.collection("budgets").updateOne(
     { _id: new ObjectId(id) },
-    {
-      $set: { category, amount, month },
-    }
+    { $set: { category, amount, month } }
   );
 
   return new Response(JSON.stringify({ success: true }), {
