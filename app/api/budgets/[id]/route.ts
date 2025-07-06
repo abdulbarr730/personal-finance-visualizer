2c569a41c62367faf9f2ee54123646c2067c47eb
Abdul Barr <abdulbarr730@gmail.com>
2025-07-07 00:03:44 +0530

    stage 3

--- app/api/budgets/[id]/route.ts
+++ app/api/budgets/[id]/route.ts
@@ -2,17 +2,25 @@ import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 import { NextRequest } from "next/server";
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const client = await clientPromise;
   const db = client.db();
   const { id } = params;
 
   await db.collection("budgets").deleteOne({ _id: new ObjectId(id) });
 
-  return new Response(JSON.stringify({ success: true }));
+  return new Response(JSON.stringify({ success: true }), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const client = await clientPromise;
   const db = client.db();
   const { id } = params;
@@ -21,8 +29,12 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
 
   await db.collection("budgets").updateOne(
     { _id: new ObjectId(id) },
-    { $set: { category, amount, month } }
+    {
+      $set: { category, amount, month },
+    }
   );
 
-  return new Response(JSON.stringify({ success: true }));
+  return new Response(JSON.stringify({ success: true }), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
