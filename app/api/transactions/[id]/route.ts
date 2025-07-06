fed7381860a3ccfcd0c2e32289356033c4b94528
Abdul Barr <abdulbarr730@gmail.com>
2025-07-06 22:27:27 +0530

    Stage 2: Added categories, dashboard, pie chart

--- app/api/transactions/[id]/route.ts
+++ app/api/transactions/[id]/route.ts
@@ -1,15 +1,17 @@
-import { NextResponse } from "next/server";
+// app/api/transactions/[id]/route.ts
+import { NextRequest, NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 
 export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: { id: string } }
 ) {
   const client = await clientPromise;
   const db = client.db("finance");
-  const id = params.id;
+  const id = context.params.id;
 
   await db.collection("transactions").deleteOne({ _id: new ObjectId(id) });
+
   return NextResponse.json({ message: "Transaction deleted" });
 }
